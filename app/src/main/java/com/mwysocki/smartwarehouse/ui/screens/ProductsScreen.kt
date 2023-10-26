3b4aed7228016795fedcdca7e81826e66bbb4244
Michał Wysocki <01150906@pw.edu.pl>
2023-10-26 17:03:41 +0200

    add adding pruducts to database

--- app/src/main/java/com/mwysocki/smartwarehouse/ui/screens/ProductsScreen.kt
+++ app/src/main/java/com/mwysocki/smartwarehouse/ui/screens/ProductsScreen.kt
@@ -9,8 +9,15 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.mwysocki.smartwarehouse.viewmodels.Product
 import com.mwysocki.smartwarehouse.viewmodels.ProductsViewModel
@@ -19,18 +26,36 @@ import com.mwysocki.smartwarehouse.viewmodels.ProductsViewModel
 fun ProductsScreen(productsViewModel: ProductsViewModel = viewModel()) {
     val productsState by productsViewModel.productsState.collectAsState()
     val searchQuery by productsViewModel.searchQuery.collectAsState()
+    val showDialog by productsViewModel.showAddProductDialog.collectAsState()
 
     Column(modifier = Modifier.fillMaxSize()) {
-        OutlinedTextField(
-            value = searchQuery,
-            onValueChange = {
-                productsViewModel.setSearchQuery(it)
-            },
-            label = { Text("Search") },
+
+        // Create a Row for the Search bar and Add button
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
-        )
+                .padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically // This ensures all items inside the Row are vertically centered
+        ) {
+            OutlinedTextField(
+                value = searchQuery,
+                onValueChange = {
+                    productsViewModel.setSearchQuery(it)
+                },
+                label = { Text("Search") },
+                modifier = Modifier.weight(1f)  // Assign weight to make it take up available space
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))  // Spacer for some space between TextField and Button
+
+            Button(
+                onClick = { productsViewModel.showAddProductDialog() },
+                modifier = Modifier.defaultMinSize(minWidth = 56.dp)
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add Product")
+            }
+        }
 
         Text(
             text = "Total products: ${productsState.allProducts.size}",
@@ -39,10 +64,28 @@ fun ProductsScreen(productsViewModel: ProductsViewModel = viewModel()) {
                 .padding(16.dp),
         )
 
-        LazyColumn {
-            items(productsState.filteredProducts) { product ->
-                ProductItem(product)
+        if (productsState.errorMessage != null) {
+            Text(
+                text = "Error: ${productsState.errorMessage}",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                color = Color.Red,
+            )
+        } else {
+            LazyColumn {
+                items(productsState.filteredProducts) { product ->
+                    ProductItem(product)
+                }
             }
+        }
+
+        // Displaying the Add Product dialog when showDialog is true
+        if (showDialog) {
+            ProductAddDialog(
+                onAddProduct = productsViewModel::addProductToFirestore,
+                onDismiss = productsViewModel::hideAddProductDialog
+            )
         }
     }
 }
@@ -60,7 +103,46 @@ fun ProductItem(product: Product) {
         }
     }
 }
+@Composable
+fun ProductAddDialog(
+    onAddProduct: (String, String) -> Unit,
+    onDismiss: () -> Unit
+) {
+    var productName by remember { mutableStateOf("") }
+    var productDescription by remember { mutableStateOf("") }
 
+    AlertDialog(
+        title = { Text("Add New Product") },
+        text = {
+            Column {
+                TextField(
+                    value = productName,
+                    onValueChange = { productName = it },
+                    label = { Text("Product Name") }
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = productDescription,
+                    onValueChange = { productDescription = it },
+                    label = { Text("Product Description") }
+                )
+            }
+        },
+        confirmButton = {
+            Button(onClick = {
+                onAddProduct(productName, productDescription)
+            }) {
+                Text("Add")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onDismiss) {
+                Text("Cancel")
+            }
+        },
+        onDismissRequest = onDismiss
+    )
+}
 @Preview(showBackground = true)
 @Composable
 fun PreviewProductsScreen() {
--- app/src/main/java/com/mwysocki/smartwarehouse/viewmodels/ProductsViewModel.kt
+++ app/src/main/java/com/mwysocki/smartwarehouse/viewmodels/ProductsViewModel.kt
@@ -16,7 +16,8 @@ data class Product(
 
 data class ProductsState(
     val allProducts: List<Product> = listOf(),
-    val filteredProducts: List<Product> = listOf()
+    val filteredProducts: List<Product> = listOf(),
+    val errorMessage: String? = null
 )
 
 class ProductsViewModel : ViewModel() {
@@ -26,6 +27,8 @@ class ProductsViewModel : ViewModel() {
     private val _searchQuery = MutableStateFlow("")
     val searchQuery: StateFlow<String> = _searchQuery.asStateFlow()
 
+    private val _showAddProductDialog = MutableStateFlow(false)
+    val showAddProductDialog: StateFlow<Boolean> = _showAddProductDialog.asStateFlow()
     init {
         fetchProducts()
     }
@@ -37,14 +40,22 @@ class ProductsViewModel : ViewModel() {
 
     private fun fetchProducts() {
         viewModelScope.launch {
-            val productList = mutableListOf<Product>()
-            FirebaseFirestore.getInstance().collection("products").get().addOnSuccessListener { result ->
-                for (document in result) {
-                    val product = document.toObject(Product::class.java)
-                    productList.add(product)
-                }
-                _productsState.value = ProductsState(allProducts = productList)
-                filterProducts()
+            try {
+                val productList = mutableListOf<Product>()
+                FirebaseFirestore.getInstance().collection("Products").get()
+                    .addOnSuccessListener { result ->
+                        for (document in result) {
+                            val product = document.toObject(Product::class.java)
+                            productList.add(product)
+                        }
+                        _productsState.value = ProductsState(allProducts = productList)
+                        filterProducts()
+                    }
+                    .addOnFailureListener { exception ->
+                        _productsState.value = ProductsState(errorMessage = exception.localizedMessage)
+                    }
+            } catch (e: Exception) {
+                _productsState.value = ProductsState(errorMessage = e.localizedMessage)
             }
         }
     }
@@ -55,5 +66,29 @@ class ProductsViewModel : ViewModel() {
                     it.description.contains(_searchQuery.value, ignoreCase = true)
         }
         _productsState.value = _productsState.value.copy(filteredProducts = filteredList)
+    }
+    fun showAddProductDialog() {
+        _showAddProductDialog.value = true
+    }
+
+    fun hideAddProductDialog() {
+        _showAddProductDialog.value = false
+    }
+
+    fun addProductToFirestore(productName: String, productDescription: String) {
+        val newProduct = mapOf(
+            "name" to productName,
+            "description" to productDescription
+            // "qrCode" can be added later as needed
+        )
+
+        FirebaseFirestore.getInstance().collection("Products").add(newProduct)
+            .addOnSuccessListener {
+                fetchProducts() // Refresh the list after adding a new product
+                hideAddProductDialog()
+            }
+            .addOnFailureListener { exception ->
+                _productsState.value = ProductsState(errorMessage = exception.localizedMessage)
+            }
     }
 }
